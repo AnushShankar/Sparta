eb0352f85ec7d9188da753d1e123c96f9a74b236
Anush Shankar <ashankar@BLR.ALLEGISINDIA.COM>
2016-03-07 16:19:47 +0530

    test

--- Count.java
+++ Count.java
@@ -1,50 +1,8 @@
 package multiThreading;
 
-public class Count extends Thread{
+public class Count {
 	
-	public Count(){
-		start();
-	}
-	
-	public void run(){
-		
-		try{
-			
-			for(int i=0;i<10;i++){
-			
-			System.out.println("the no is " + i);
-			Thread.sleep(1000);
-			}
-		}
-		
-		catch(InterruptedException e){
-			System.out.println("myThread has been interrupted");
-		}
-		
-			System.out.println("myThread has run over");
-	}
-
-}
-
-
-class ExtendingExample
-{
-	public static void main(string args[])
-	{
-		Count cnt = new Count();
-		
-		try
-		{
-			while(cnt.isAlive())
-			{
-				System.out.println("Main Thread");
-				Thread.sleep(1000);
-			}
-		}
-		
-		catch(InterruptedException e)
-		{
-			System.out.println("Main thread is interrupted");
-		}
+	public static void main(String args[]){
+		System.out.println("hello");
 	}
 }
