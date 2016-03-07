9dce0059a90d756d3369dd648de983042ca95ca7
Anush Shankar <ashankar@BLR.ALLEGISINDIA.COM>
2016-03-07 17:52:39 +0530

    test

--- Count.java
+++ Count.java
@@ -1,8 +1,19 @@
 package multiThreading;
 
+import java.util.Scanner;
+
 public class Count {
+	count(){
+		System.out.println("enter the inputs");
+		
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		System.out.println(n);
+	}
 	
 	public static void main(String args[]){
-		System.out.println("hello");
+		Count cnt = new Count();
+		
+		
 	}
 }
